31ef0dc130c25a163b0bf7467368fe4faacb5110
张凯荣 <zhangkr@dehuinet.com>
2020-06-12 10:08:03 +0800

    1.两数合

--- 简单/1.两数合.js
+++ 简单/1.两数合.js
@@ -1,20 +1,15 @@
-const nums = [3,2,4]
+const question = `给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。`;
 
-const target = 6
-
-
-var test = function (nums, target) {
-  let res = []
-  for (let index = 0; index < nums.length; index++) {
-    let goal = target - nums[index]
-    if (nums.indexOf(goal) > -1 && nums[index] !== nums[nums.indexOf(goal)]) {
-      res.push(index)
-      res.push(nums.indexOf(goal))
-      break;
-    }
+var twoSum = function(nums, target) {
+  let i = nums.length;
+  while(i > 1) {
+      let last = nums.pop();
+      if (nums.indexOf(target - last) > -1) {
+          return [nums.indexOf(target - last), nums.length]
+      }
+      i--
   }
-  console.log(res);
-  
-}
+};
 
-test(nums, target);
+console.log(twoSum([3,2,4],6));
