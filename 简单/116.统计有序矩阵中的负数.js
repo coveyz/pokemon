1817f822a37fab9cbf745b92c32c836933ea367b
张凯荣 <zhangkr@dehuinet.com>
2020-07-02 17:49:33 +0800

    easy 116.统计有序矩阵中的负数

---
+++ 简单/116.统计有序矩阵中的负数.js
@@ -0,0 +1,37 @@
+const question = `给你一个 m * n 的矩阵 grid，矩阵中的元素无论是按行还是按列，都以非递增顺序排列。 
+请你统计并返回 grid 中 负数 的数目。`;
+
+const countNegatives = (grid) => {
+	return (negativesIndex = grid.reduce((acc, cur) => {
+		if (cur[0] < 0 && cur[cur.length - 1] < 0) {
+			acc += cur.length;
+		} else {
+			for (let index = 0; index < cur.length; index++) {
+				const element = cur[index];
+				if (element < 0) acc += 1;
+			}
+		}
+
+		return acc;
+	}, 0));
+};
+
+console.log(
+	countNegatives([
+		[4, 3, 2, -1],
+		[3, 2, 1, -1],
+		[1, 1, -1, -2],
+		[-1, -1, -2, -3],
+	])
+);
+
+console.log(
+	countNegatives([
+    [[3,2],[1,0]]
+	])
+);
+
+
+console.log(
+	countNegatives([[-1]])
+);
