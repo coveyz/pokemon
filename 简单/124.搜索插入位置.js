e1b9dfa05031d575251438675d014d7a7e3fa626
张凯荣 <zhangkr@dehuinet.com>
2020-07-17 09:58:00 +0800

    easy 124.搜索插入位置

---
+++ 简单/124.搜索插入位置.js
@@ -0,0 +1,17 @@
+const question = `给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+你可以假设数组中无重复元素。`;
+
+const searchInsert = (nums, target) => {
+	for (let index = 0; index < nums.length; index++) {
+		const element = nums[index];
+		if (element >= target) {
+			return index;
+		}
+	}
+	return nums.length;
+};
+
+console.log(searchInsert([1, 3, 5, 6], 5));
+console.log(searchInsert([1, 3, 5, 6], 2));
+console.log(searchInsert([1, 3, 5, 6], 7));
+console.log(searchInsert([1, 3, 5, 6], 0));
