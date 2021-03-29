059173780b5cc8c1327688d7992e5b5ef25ae4c3
张凯荣 <zhangkr@dehuinet.com>
2021-03-29 10:25:08 +0800

    middle 52.根据字符出现频率排序

---
+++ 中等/52.根据字符出现频率排序.js
@@ -0,0 +1,26 @@
+const question = `
+给定一个字符串，请将字符串里的字符按照出现的频率降序排列。
+`;
+
+const frequencySort = (s) => {
+	const obj = {};
+
+	for (let index = 0; index < s.length; index++) {
+		const element = s[index];
+		obj[element] = obj[element] ? obj[element] + element : element;
+	}
+
+	return Object.values(obj)
+		.sort((a, b) => {
+			return b.length - a.length;
+		})
+		.reduce((acc, cur) => {
+			acc += cur;
+			return acc;
+		}, '');
+};
+
+// console.log(frequencySort('tree'));
+// console.log(frequencySort('cccaaa'));
+// console.log(frequencySort('Aabb'));
+console.log(frequencySort('loveleetcode'));
