a406223bf5906e631fb1fc63d5686a986b24bdab
张凯荣 <zhangkr@dehuinet.com>
2021-03-31 09:04:09 +0800

    middle 57.子集 II

---
+++ 中等/57.子集 II.js
@@ -0,0 +1,29 @@
+const question = `
+给你一个整数数组 nums ，其中可能包含重复元素，请你返回该数组所有可能的子集（幂集）。
+解集 不能 包含重复的子集。返回的解集中，子集可以按 任意顺序 排列。
+`;
+
+const subsetsWithDup = (nums) => {
+	nums.sort((a, b) => a - b);
+
+	let t = [],
+		ans = [];
+
+	const dfs = (choosePre, cur, nums) => {
+		if (cur === nums.length) {
+			ans.push(t.slice());
+			return;
+		}
+		dfs(false, cur + 1, nums);
+		if (!choosePre && cur && nums[cur - 1] === nums[cur]) {
+			return;
+		}
+		t.push([nums[cur]]);
+		dfs(true, cur + 1, nums);
+		t = t.slice(0, t.length - 1);
+	};
+	dfs(true, 0, nums);
+	return ans;
+};
+
+console.log(subsetsWithDup([1, 2, 2]));
