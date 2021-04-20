93a756990cbf7baaf34049cd3e1521d96561bc6f
张凯荣 <zhangkr@dehuinet.com>
2021-04-20 16:33:02 +0800

    middle 75.找出克隆二叉树中的相同节点

---
+++ 中等/75.找出克隆二叉树中的相同节点.js
@@ -0,0 +1,34 @@
+const question = `
+给你两棵二叉树，原始树 original 和克隆树 cloned，以及一个位于原始树 original 中的目标节点 target。
+其中，克隆树 cloned 是原始树 original 的一个 副本 。
+请找出在树 cloned 中，与 target 相同 的节点，并返回对该节点的引用（在 C/C++ 等有指针的语言中返回 节点指针，其他语言返回节点本身）。
+`;
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} original
+ * @param {TreeNode} cloned
+ * @param {TreeNode} target
+ * @return {TreeNode}
+ */
+
+var getTargetCopy = function (original, cloned, target) {
+	const queue = [];
+	let res;
+	queue.push(cloned);
+
+	while (queue.length) {
+		const node = queue.shift();
+		if (node.val === target.val) {
+			return node;
+		} else {
+			node.left && queue.push(node.left);
+			node.right && queue.push(node.right);
+		}
+	}
+};
