1baa847c7c151fd4a30d104255f398b02653030e
张凯荣 <zhangkr@dehuinet.com>
2021-04-22 11:53:20 +0800

    easy 188.最大升序子数组和

--- 中等/78.N 叉树的层序遍历.js
+++ 中等/78.N 叉树的层序遍历.js
@@ -2,13 +2,6 @@ const question = `
 给定一个 N 叉树，返回其节点值的层序遍历。（即从左到右，逐层遍历）。
 树的序列化输入是用层序遍历，每组子节点都由 null 值分隔（参见示例）
 `;
-/**
- * // Definition for a Node.
- * function Node(val,children) {
- *    this.val = val;
- *    this.children = children;
- * };
- */
 
 /**
  * @param {Node} root
---
+++ 简单/188.最大升序子数组和.js
@@ -0,0 +1,44 @@
+const question = `
+给你一个正整数组成的数组 nums ，返回 nums 中一个 升序 子数组的最大可能元素和。
+子数组是数组中的一个连续数字序列。
+已知子数组 [numsl, numsl+1, ..., numsr-1, numsr] ，若对所有 i（l <= i < r），numsi < numsi+1 都成立，则称这一子数组为 升序 子数组。注意，大小为 1 的子数组也视作 升序 子数组。
+`;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxAscendingSum = function (nums) {
+	const len = nums.length;
+	let count = 0;
+	for (let index = 0; index < len; index++) {
+		const element = nums[index];
+		const qq = [element];
+		for (let key = index + 1; key < len; key++) {
+			const item = nums[key];
+			if (element < item) {
+				if (item < nums[key + 1]) {
+					qq.push(item);
+				} else if (!nums[key + 1]) {
+					qq.push(item);
+				} else if (item >= nums[key + 1]) {
+					qq.push(item);
+					break;
+				}
+			} else {
+				break;
+			}
+		}
+		const regionSum = qq.reduce((acc, cur) => acc + cur, 0);
+		count = Math.max(regionSum, count);
+	}
+
+	return count;
+};
+
+console.log(maxAscendingSum([10, 20, 30, 5, 10, 50]));
+console.log(maxAscendingSum([10, 20, 30, 40, 50]));
+console.log(maxAscendingSum([12, 17, 15, 13, 10, 11, 12]));
+console.log(maxAscendingSum([100, 1]));
+console.log(maxAscendingSum([6, 10, 6]));
+console.log(maxAscendingSum([3, 6, 10, 1, 8, 9, 9, 8, 9]));
+console.log(maxAscendingSum([9, 10]));
