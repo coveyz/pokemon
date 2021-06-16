93b78b1edb271144f8a10ce5307bdb4c37a1e3a2
张凯荣 <zhangkr@dehuinet.com>
2021-06-16 10:05:54 +0800

    middle 96.完全平方数

--- 中等/95.有重复字符串的排列组合.js
+++ 中等/95.有重复字符串的排列组合.js
@@ -8,13 +8,17 @@ var permutation = function (S) {
 	const arr = S.split('').sort();
 	const res = [];
 
+	//* 使用两个集合  1-已经遍历的 2-未遍历的
+	//* 未遍历为空的时候 返回
 	const brackTrack = (track, store) => {
 		if (store.length === 0) {
 			res.push(track.join(''));
 			return;
 		}
+		//* 1.0 只需要遍历未遍历的集合
 		for (let index = 0; index < store.length; index++) {
 			const element = store[index];
+			//* 2.0 如果当前元素 和上一个元素一样 直接跳过
 			if (store[index - 1] === element) continue;
 			track.push(element);
 			brackTrack(
@@ -30,6 +34,6 @@ var permutation = function (S) {
 	return res;
 };
 
-console.log(permutation('qqe'));
+// console.log(permutation('qqe'));
 console.log(permutation('Frx'));
-console.log(permutation('ab'));
+// console.log(permutation('ab'));
