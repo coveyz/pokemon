2e96d09804e452bbe3cb22994c7fbbc2711c590e
张凯荣 <zhangkr@dehuinet.com>
2021-06-23 16:06:24 +0800

    easy 221. 员工的重要性

---
+++ 简单/221. 员工的重要性.js
@@ -0,0 +1,53 @@
+const question = `
+给定一个保存员工信息的数据结构，它包含了员工 唯一的 id ，重要度 和 直系下属的 id 。
+比如，员工 1 是员工 2 的领导，员工 2 是员工 3 的领导。他们相应的重要度为 15 , 10 , 5 。那么员工 1 的数据结构是 [1, 15, [2]] ，员工 2的 数据结构是 [2, 10, [3]] ，员工 3 的数据结构是 [3, 5, []] 。注意虽然员工 3 也是员工 1 的一个下属，但是由于 并不是直系 下属，因此没有体现在员工 1 的数据结构中。
+现在输入一个公司的所有员工信息，以及单个员工 id ，返回这个员工和他所有下属的重要度之和。
+`;
+
+/**
+ * Definition for Employee.
+ * function Employee(id, importance, subordinates) {
+ *     this.id = id;
+ *     this.importance = importance;
+ *     this.subordinates = subordinates;
+ * }
+ */
+
+/**
+ * @param {Employee[]} employees
+ * @param {number} id
+ * @return {number}
+ */
+var GetImportance = function (employees, id) {
+	// employees.sort((a, b) => a[0] - b[0]); //* 先根据员工ID 给排序
+
+	const obj = {};
+
+	for (const employee of employees) {
+		obj[employee.id] = employee;
+	}
+
+	const dps = (id) => {
+		const employee = obj[id];
+		let total = employee.importance;
+		const subordinates = employee.subordinates;
+
+		for (const subId of subordinates) {
+			total += dps(subId);
+		}
+		return dps;
+	};
+
+	return dps(id);
+};
+
+console.log(
+	GetImportance(
+		[
+			[1, 5, [2, 3]],
+			[3, 3, []],
+			[2, 3, []],
+		],
+		1
+	)
+);
