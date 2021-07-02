1e5bc6f2c312736e6196ba5d6f8cd3293657230f
张凯荣 <zhangkr@dehuinet.com>
2021-07-02 09:42:38 +0800

    easy 222.翻转二叉树

---
+++ 简单/222.翻转二叉树.js
@@ -0,0 +1,30 @@
+const question = `
+翻转一棵二叉树。
+`
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+  if (!root) {
+    return null
+  }
+
+  const left = invertTree(root.left)
+  const right = invertTree(root.right)
+
+  root.left = right
+  root.right = left
+
+  return root
+
+};
