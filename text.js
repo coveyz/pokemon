abcb635da16d95d68e8ace3c748c4b6f794b1e5f
张凯荣 <zhangkr@dehuinet.com>
2021-07-09 10:11:44 +0800

    middle  102.删除链表的倒数第 N 个结点

--- text.js
+++
@@ -1,9 +0,0 @@
-const magic = (arr) => {
-	return arr.sort((a, b) => {
-		if (a === 0) {
-			return a - b;
-		}
-	});
-};
-
-console.log(magic([0, 2, 1, 0, 3, 6, 7, 1, 0, 3]));
---
+++ 中等/102. 删除链表的倒数第 N 个结点.js
@@ -0,0 +1,48 @@
+const question = `
+  给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+  进阶：你能尝试使用一趟扫描实现吗？
+`;
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function (head, n) {
+	let cur = head;
+	let len = 0;
+
+	while (cur) {
+		cur = cur.next;
+		len += 1;
+	}
+	let del_node = null;
+
+	if (len === 1 || len - n === 0) {
+		del_node = head;
+		head = head.next;
+		return head;
+	} else {
+		let del_index = 0;
+		let pre_node = null;
+		let cur_node = head;
+
+		while (del_index < len - n) {
+			pre_node = cur_node;
+			cur_node = cur_node.next;
+			del_index += 1;
+		}
+
+		del_node = cur_node;
+		pre_node.next = cur_node.next;
+		del_node.next = null;
+
+		return head;
+	}
+};
