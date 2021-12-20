ad871758f93b3c34f7e8ecef0b9ff0dfcf6c3720
张凯荣 <zhangkr@dehuinet.com>
2021-12-20 11:36:34 +0800

    easy 22.有序数组的平方->  双指针

--- 简单/22.有序数组的平方.js
+++ 简单/22.有序数组的平方.js
@@ -1,7 +1,33 @@
-const question = `给定一个按非递减顺序排序的整数数组 A，返回每个数字的平方组成的新数组，要求也按非递减顺序排序。`
+const question = `给定一个按非递减顺序排序的整数数组 A，返回每个数字的平方组成的新数组，要求也按非递减顺序排序。`;
 
-const sortedSquares = (A) => {
-  return A.map(item => Math.pow(item,2)).sort((a,b) => a-b)
-}
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function (nums) {
+	let len = nums.length;
+	let arr = new Array(len);
+	let left = 0,
+		right = len - 1,
+		pos = len - 1;
 
-console.log(sortedSquares([-7,-3,2,3,11]));
+	while (left <= right) {
+		let leftSq = nums[left] * nums[left];
+		let rightSq = nums[right] * nums[right];
+
+		if (leftSq > rightSq) {
+			arr[pos] = leftSq;
+			left += 1;
+		} else {
+			arr[pos] = rightSq;
+			right -= 1;
+		}
+
+		pos -= 1;
+	}
+
+	return arr;
+};
+
+console.log(sortedSquares([-4, -1, 0, 3, 10]));
+console.log(sortedSquares([-7, -3, 2, 3, 11]));
