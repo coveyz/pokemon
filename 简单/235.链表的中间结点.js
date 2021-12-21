72656864c4e8061bd514c3f6121473928a9de219
张凯荣 <zhangkr@dehuinet.com>
2021-12-21 15:58:04 +0800

    easy 235.链表的中间结点

---
+++ 简单/235.链表的中间结点.js
@@ -0,0 +1,26 @@
+const question = `
+给定一个头结点为 head 的非空单链表，返回链表的中间结点。
+如果有两个中间结点，则返回第二个中间结点。
+`;
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function (head) {
+	//* 快慢指针
+	let slow = (fast = head);
+
+	while (fast && fast.next) {
+		slow = slow.next;
+		fast = fast.next.next;
+	}
+	return slow;
+};
