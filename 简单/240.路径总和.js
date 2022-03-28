18d2837b0116f86131d5076b2f4068447ddb93c8
张凯荣 <zhangkr@dehuinet.com>
2022-03-28 11:29:04 +0800

    easy 240.路径总和

---
+++ 简单/240.路径总和.js
@@ -0,0 +1,26 @@
+const question = `
+给你二叉树的根节点 root 和一个表示目标和的整数 targetSum 。判断该树中是否存在 根节点到叶子节点 的路径，这条路径上所有节点值相加等于目标和 targetSum 。如果存在，返回 true ；否则，返回 false 。
+叶子节点 是指没有子节点的节点。
+`;
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {boolean}
+ */
+var hasPathSum = function (root, sum) {
+	if (!root) return false;
+
+	if (root.left === null && root.right === null) {
+		return sum - root.val === 0;
+	}
+
+	return hasPathSum(root.left, (sum -= root.left)) || hasPathSum(root.right, (sum -= root.right));
+};
