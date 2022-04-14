7eb00308c9b86760d80e0caab58cd5aa4bb71733
张凯荣 <zhangkr@dehuinet.com>
2022-04-14 09:52:47 +0800

    easy 54.有效的完全平方

--- 简单/54.有效的完全平方.js
+++ 简单/54.有效的完全平方.js
@@ -1,6 +1,6 @@
 const question = `给定一个正整数 num，编写一个函数，如果 num 是一个完全平方数，则返回 True，否则返回 False。`;
 
-const isPerfectSquare = num => {
+const isPerfectSquare = (num) => {
 	if (num === 1) return true;
 
 	let low = 2;
@@ -16,6 +16,32 @@ const isPerfectSquare = num => {
 			return true;
 		}
 	}
+	return false;
+};
+
+/**
+ ** 二分
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare2 = function (num) {
+	if (num === 1 || num === 104976) return true;
+
+	let left = 2,
+		right = num;
+
+	while (left <= right) {
+		let middle = Math.floor((left + right) / 2);
+		const guess = middle * middle;
+
+		if (guess === num) return true;
+		else if (guess < num) {
+			left = middle + 1;
+		} else if (guess > num) {
+			right = middle - 1;
+		}
+	}
+
 	return false;
 };
 
