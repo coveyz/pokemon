5e1a93dda4fc7b42bb594e162e305d7fa4e23522
张凯荣 <zhangkr@dehuinet.com>
2022-04-18 10:33:11 +0800

    easy 243. 特殊数组的特征值

--- 简单/125.两数之和 II - 输入有序数组.js
+++ 简单/125.两数之和 II - 输入有序数组.js
@@ -20,6 +20,12 @@ const question = `定一个已按照升序排列 的有序数组，找到两个
 // 	return res;
 // };
 
+/**
+ ** 二分查找
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
 var twoSum = function (numbers, target) {
 	let left = 0,
 		right = numbers.length - 1;
@@ -37,6 +43,27 @@ var twoSum = function (numbers, target) {
 	}
 
 	return -1;
+};
+
+
+/**
+ ** Map
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+ var twoSum = function (numbers, target) {
+	const map = new Map();
+
+	for (let index = 0; index < numbers.length; index++) {
+		const element = numbers[index];
+		let result = target - element;
+		if (map.has(element)) {
+			return [ map.get(element)+1,index + 1,];
+		} else {
+			map.set(result, index);
+		}
+	}
 };
 
 console.log(twoSum([2, 7, 11, 15], 9));
