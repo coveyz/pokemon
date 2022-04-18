d9710d4814c8190ab22abc56c2fbc8044e8ecf36
张凯荣 <zhangkr@dehuinet.com>
2022-04-18 09:53:56 +0800

    easy 242. 第 k 个缺失的正整数

---
+++ 简单/242. 第 k 个缺失的正整数.js
@@ -0,0 +1,59 @@
+const question = `
+ 给你一个 严格升序排列 的正整数数组 arr 和一个整数 k 。
+请你找到这个数组里第 k 个缺失的正整数。
+ `;
+
+/**
+ ** 暴力
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+var findKthPositive1 = function (arr, k) {
+	const max_length = arr[arr.length - 1];
+	const new_arr = new Array(max_length)
+		.fill(1)
+		.map((_, index) => index + 1)
+		.filter((item) => !arr.includes(item));
+
+	const len = new_arr.length;
+
+	// console.log('len=>', len, 'k=>', k, new_arr);
+	if (len >= k) {
+		return new_arr[k - 1];
+	} else {
+		const need = k - len;
+		// console.log('need=>', need);
+		return max_length + need;
+	}
+};
+
+/**
+ ** 二分查找
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+var findKthPositive = function (arr, k) {
+	if (arr[0] > k) return k;
+
+	let left = 0,
+		right = arr.length;
+
+	while (left < right) {
+		const middle = Math.floor((left + right) / 2);
+		let x = middle < arr.length ? arr[middle] : 2000000;
+
+		if (x - middle - 1 >= k) {
+			right = middle;
+		} else {
+			left = middle + 1;
+		}
+	}
+	// return { left, right };
+	return k - (arr[left - 1] - (left - 1) - 1) + arr[left - 1];
+};
+
+let arr = [2, 3, 4, 7, 11],
+	k = 5;
+console.log(findKthPositive(arr, k));
--- 简单/65.缺失数字.js
+++ 简单/65.缺失数字.js
@@ -1,6 +1,6 @@
 const question = `给定一个包含 0, 1, 2, ..., n 中 n 个数的序列，找出 0 .. n 中没有出现在序列中的那个数。`;
 
-const missingNumber = nums => {
+const missingNumber = (nums) => {
 	var newSort = nums.sort((a, b) => a - b);
 	const fistNum = newSort[0];
 	const endNum = newSort[newSort.length - 1];
@@ -24,7 +24,7 @@ const missingNumber = nums => {
     let total = (len * (len + 1)) / 2
  * @param {*} nums 
  */
-const missingNumber1 = nums => {
+const missingNumber1 = (nums) => {
 	let len = nums.length;
 	let total = (len * (len + 1)) / 2;
 	const curNumsTotal = nums.reduce((acc, cur) => (acc += cur), 0);
