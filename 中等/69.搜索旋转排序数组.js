8541ad2a642f76958f794dbecc5696dc77a2c565
张凯荣 <zhangkr@dehuinet.com>
2022-04-22 09:01:45 +0800

    easy 69.搜索旋转排序数组 -> 二分查找

--- 中等/69.搜索旋转排序数组.js
+++ 中等/69.搜索旋转排序数组.js
@@ -4,13 +4,50 @@ const question = `
 在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转，使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。例如， [0,1,2,4,5,6,7] 在下标 3 处经旋转后可能变为 [4,5,6,7,0,1,2] 。
 
 给你 旋转后 的数组 nums 和一个整数 target ，如果 nums 中存在这个目标值 target ，则返回它的下标，否则返回 -1 。
-`
+`;
 
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number}
  */
-var search = function(nums, target) {
-    return nums.indexOf(target)
+var search1 = function (nums, target) {
+	return nums.indexOf(target);
+};
+
+/**
+ ** 二分查找
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+	let len = nums.length;
+	if (!len) return -1;
+	if (len === 1) return nums[0] === target ? 0 : -1;
+
+	let left = 0,
+		right = len - 1;
+
+	while (left <= right) {
+		let middle = Math.floor((left + right) / 2);
+		// let middle = (left + right) / 2
+		if (nums[middle] === target) return middle;
+
+		if (nums[0] <= nums[middle]) {
+			if (nums[0] <= target && target < nums[middle]) {
+				right = middle - 1;
+			} else {
+				left = middle + 1;
+			}
+		} else {
+			if (nums[middle] < target && target <= nums[len - 1]) {
+				left = middle + 1;
+			} else {
+				right = middle - 1;
+			}
+		}
+	}
+
+	return -1;
 };
