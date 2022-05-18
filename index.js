441d8e3b9551bdca121d124593b708947a5a284f
covey <zhangkairong123@qq.com>
2022-05-18 09:43:46 +0800

    middle 5.寻找峰值 -> 二分查找

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
-const question = `峰值元素是指其值大于左右相邻值的元素。
-给定一个输入数组 nums，其中 nums[i] ≠ nums[i+1]，找到峰值元素并返回其索引。
-数组可能包含多个峰值，在这种情况下，返回任何一个峰值所在位置即可。
-你可以假设 nums[-1] = nums[n] = -∞。`;
-
-const findPeakElement = (A) => {
-	return A.indexOf(Math.max(...A));
-};
-
-console.log(findPeakElement([1, 2, 3, 1]));
-
 /**
- ** 二分查找
  * @param {number[]} nums
  * @return {number}
  */
-var findPeakElement2 = function (nums) {
+var findPeakElement = function (nums) {
 	const n = nums.length;
 	let left = 0,
 		right = n - 1,
@@ -59,3 +47,7 @@ var findPeakElement2 = function (nums) {
 
 	return ans;
 };
+
+const nums = [1, 2, 1, 3, 7, 5, 7, 6, 4];
+
+console.log(findPeakElement(nums));
