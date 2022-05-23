00607b39302b457cb157f056307feccda26a2b51
covey <zhangkairong123@qq.com>
2022-05-23 10:20:17 +0800

    easy 245.合并两个有序链表 -> 递归

--- 简单/245.合并两个有序链表.js
+++ 简单/245.合并两个有序链表.js
@@ -10,6 +10,7 @@ const question = `
  * }
  */
 /**
+ ** 迭代
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
@@ -62,4 +63,29 @@ var mergeTwoLists = function (l1, l2) {
 	}
 
 	return merge_head;
+};
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ ** 递归
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+	if (list1 == null) return list2;
+	else if (list2 === null) return list1;
+	else if (list1.val < list2.val) {
+		list1.next = mergeTwoLists(list1.next, list2);
+		return list1;
+	} else {
+		list2.next = mergeTwoLists(list1, list2.next);
+		return list2;
+	}
 };
