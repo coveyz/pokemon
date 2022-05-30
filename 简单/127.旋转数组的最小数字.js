37bdb0ee9351340473f0b9d60f6480ad5fa1caea
covey <zhangkairong123@qq.com>
2022-05-30 09:55:59 +0800

    easy 127.旋转数组的最小数字 -> 数字

--- 简单/127.旋转数组的最小数字.js
+++ 简单/127.旋转数组的最小数字.js
@@ -4,4 +4,13 @@ const minArray = (numbers) => {
 	return numbers.sort((a, b) => a - b)[0];
 };
 
-console.log(minArray([3,4,5,1,2]))
+/**
+ ** Math方法
+ * @param {number[]} numbers
+ * @return {number}
+ */
+var minArray2 = function (numbers) {
+	return Math.min(...numbers);
+};
+
+console.log(minArray([3, 4, 5, 1, 2]));
