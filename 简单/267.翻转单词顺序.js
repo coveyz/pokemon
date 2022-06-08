856787a80d0ef139232af8e1a65bcbe1ee5f8726
covey <zhangkairong123@qq.com>
2022-06-08 13:59:02 +0800

    easy 267.翻转单词顺序

---
+++ 简单/267.翻转单词顺序.js
@@ -0,0 +1,25 @@
+const question = `
+输入一个英文句子，翻转句子中单词的顺序，但单词内字符的顺序不变。为简单起见，标点符号和普通字母一样处理。例如输入字符串"I am a student. "，则输出"student. a am I"。
+`;
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function (s) {
+	const arr = s.trim().split(' ').reverse(),
+		len = arr.length;
+	return arr.reduce((acc, cur, index) => {
+		if (cur) {
+			acc = acc + cur + (index === len - 1 ? '' : ' ');
+		}
+
+		return acc;
+	}, '');
+};
+
+// const str = 'the sky is blue';
+// const str = '  hello world!  ';
+const str = 'a good   example';
+
+console.log(reverseWords(str));
