324730ad8c202e66bb244fc481b687e5a99056f2
covey <zhangkairong123@qq.com>
2022-06-20 10:16:18 +0800

    easy 201.调整数组顺序使奇数位于偶数前面 -> 双数组

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
 /**
  * @param {number[]} nums
- * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {
-	const map = new Map();
+var exchange = function (nums) {
+	const odd_arr = [],
+		even_arr = [];
 
 	for (let index = 0; index < nums.length; index++) {
-		const element = nums[index],
-			need_item = target - element;
-		console.log(map.has(need_item), element, need_item);
-		if (map.has(element)) {
-			return [element, map.get(element)];
+		const element = nums[index];
+
+		if (element % 2) {
+			odd_arr.push(element);
 		} else {
-			map.set(need_item, element);
+			even_arr.push(element);
 		}
 	}
 
-	return map;
+	return odd_arr.concat(even_arr);
 };
 
-// const nums = [2, 7, 11, 15],
-// 	target = 9;
+const nums = [1, 2, 3, 4];
 
-const nums = [10, 26, 30, 31, 47, 60],
-	target = 40;
-
-console.log(twoSum(nums, target));
+console.log(exchange(nums));
--- 简单/201.调整数组顺序使奇数位于偶数前面.js
+++ 简单/201.调整数组顺序使奇数位于偶数前面.js
@@ -12,4 +12,26 @@ var exchange = function (nums) {
 	});
 };
 
+/**
+ ** 双数组
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var exchange = function (nums) {
+	const odd_arr = [],
+		even_arr = [];
+
+	for (let index = 0; index < nums.length; index++) {
+		const element = nums[index];
+
+		if (element % 2) {
+			odd_arr.push(element);
+		} else {
+			even_arr.push(element);
+		}
+	}
+
+	return odd_arr.concat(even_arr);
+};
+
 console.log(exchange([1, 2, 3, 4]));
