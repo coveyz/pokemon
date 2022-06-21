b820d3f1703bf9efd45342faeed82b313bcc742e
covey <zhangkairong123@qq.com>
2022-06-21 18:59:01 +0800

    easy 81.IP 地址无效化 -> 替换字符串

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 /**
- * @param {number[]} nums
- * @return {number[]}
+ * @param {string} address
+ * @return {string}
  */
-var exchange = function (nums) {
-	let left = 0,
-		right = nums.length - 1;
-
-	while (left < right) {
-		while (left < right && nums[left] % 2 === 1) left++;
-		while (left < right && nums[right] % 2 === 0) right--;
-
-		[nums[left], nums[right]] = [nums[right], nums[left]];
-	}
-
-	return nums;
+var defangIPaddr = function (address) {
+	return address.replace(/\./g, '[.]');
 };
 
-const nums = [1, 2, 3, 4];
+const address = '1.1.1.1';
 
-console.log(exchange(nums));
+console.log(defangIPaddr(address));
--- 简单/81.IP 地址无效化.js
+++ 简单/81.IP 地址无效化.js
@@ -1,16 +1,25 @@
 const question = `给你一个有效的 IPv4 地址 address，返回这个 IP 地址的无效化版本。
 所谓无效化 IP 地址，其实就是用 "[.]" 代替了每个 "."。`;
 
-const defangIPaddr = address => {
+const defangIPaddr = (address) => {
 	let str = '';
 	for (let index = 0; index < address.length; index++) {
 		if (address[index] === '.') {
-			str += '[.]'
+			str += '[.]';
 		} else {
-			str += address[index]
+			str += address[index];
 		}
-  }
-  return str
+	}
+	return str;
+};
+
+/**
+ ** 替换字符串
+ * @param {string} address
+ * @return {string}
+ */
+var defangIPaddr2 = function (address) {
+	return address.replace(/\./g, '[.]');
 };
 
 console.log(defangIPaddr('1.1.1.1'));
