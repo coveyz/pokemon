9e2b7cccf3518b53d31c43f7c7873bb4b4a489cd
Saturday <zhangkairong123@qq.com>
2022-08-03 09:43:46 +0800

    easy 236.合并两个排序的链表 -> 迭代

--- 简单/236.合并两个排序的链表.js
+++ 简单/236.合并两个排序的链表.js
@@ -29,3 +29,70 @@ var mergeTwoLists = function (l1, l2) {
 		return l2;
 	}
 };
+
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ ** 迭代
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+ var mergeTwoLists = function (l1, l2) {
+	if (!l1) return l2;
+	if (!l2) return l1;
+
+	let merge_header = null,
+		merge_tail = null,
+		cur_l1 = l1,
+		cur_l2 = l2;
+
+	while (cur_l1 && cur_l2) {
+		let min_data;
+		if (cur_l1.val < cur_l2.val) {
+			min_data = cur_l1.val;
+			cur_l1 = cur_l1.next;
+		} else {
+			min_data = cur_l2.val;
+			cur_l2 = cur_l2.next;
+		}
+        
+		const new_node = new ListNode(min_data);
+        
+		if (!merge_header) {
+			merge_header = new_node;
+			merge_tail = new_node;
+		} else {
+			merge_tail.next = new_node;
+			merge_tail = new_node;
+		}
+	}
+
+	let link_str = null;
+
+	if (cur_l1) {
+		link_str = cur_l1;
+	}
+	if (cur_l2) {
+		link_str = cur_l2;
+	}
+    
+    
+
+	while (link_str) {
+		const new_node = new ListNode(link_str.val);
+		merge_tail.next = new_node;
+		merge_tail = new_node;
+		link_str = link_str.next;
+	}
+
+	return merge_header;
+};
+
