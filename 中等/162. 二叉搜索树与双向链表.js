b554052e7e847b2d366398a837cfe321e01ec3ba
Saturday <zhangkairong123@qq.com>
2022-08-20 10:33:58 +0800

    middle 162. 二叉搜索树与双向链表

---
+++ 中等/162. 二叉搜索树与双向链表.js
@@ -0,0 +1,41 @@
+/**
+ * // Definition for a Node.
+ * function Node(val,left,right) {
+ *    this.val = val;
+ *    this.left = left;
+ *    this.right = right;
+ * };
+ */
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var treeToDoublyList = function (root) {
+	/**
+	 ** 中序遍历
+	 */
+
+	if (!root) return root;
+
+	let prev = null,
+		head = null;
+
+	const dfsHelper = (root) => {
+		if (!root) return root;
+		dfsHelper(root.left);
+		if (!head) {
+			head = new Node(root.val);
+			prev = head;
+		} else {
+			let new_node = new Node(root.val);
+			prev.right = new_node;
+			new_node.left = prev;
+			prev = new_node;
+		}
+	};
+	dfsHelper(root);
+	head.left = prev;
+	prev.right = head;
+
+	return head;
+};
