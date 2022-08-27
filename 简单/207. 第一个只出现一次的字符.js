d48d18f5b7872bec13c9971d592498627ad39a01
Saturday <zhangkairong123@qq.com>
2022-08-27 10:48:04 +0800

    easy 207. 第一个只出现一次的字符 -> 哈希表

--- 简单/207. 第一个只出现一次的字符.js
+++ 简单/207. 第一个只出现一次的字符.js
@@ -1,24 +1,35 @@
+const question = `
+在字符串 s 中找出第一个只出现一次的字符。如果没有，返回一个单空格。 s 只包含小写字母。
+`;
+
 /**
  * @param {string} s
  * @return {character}
  */
 var firstUniqChar = function (s) {
-	if (s === '') return '';
-	const obj = {};
-
+	if (!s.length) return ' ';
+	const map = new Map(),
+		delMap = new Map();
 	for (let index = 0; index < s.length; index++) {
 		const element = s[index];
-		obj[element] = (obj[element] || 0) + 1;
-	}
-
-	for (let index = 0; index < s.length; index++) {
-		const element = s[index];
-		if (obj[element] === 1) {
-			return element;
+		if (delMap.has(element)) {
+			continue;
+		} else {
+			if (map.has(element)) {
+				delMap.set(element, true);
+				map.delete(element);
+			} else {
+				map.set(element, true);
+			}
 		}
 	}
 
-	return -1;
+	for (const [key, value] of map) {
+		return key;
+	}
+
+	return ' ';
 };
 
-console.log(firstUniqChar('abaccdeff'));
+const s = 'abaccdeff';
+console.log(firstUniqChar(s));
