c2bde532ca9c37f5a53a3253d26e83c5f822f295
Saturday <zhangkairong123@qq.com>
2022-11-01 10:47:12 +0800

    middle 155.礼物的最大价值 -> 动态规划

--- 中等/155.礼物的最大价值.js
+++ 中等/155.礼物的最大价值.js
@@ -31,6 +31,31 @@ var maxValue = function (grid) {
 	return dp[row - 1][col - 1];
 };
 
+/**
+ ** 动态规划
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxValue = function (grid) {
+	if (!grid.length || !grid[0].length) return 0;
+
+	let rowLimit = grid.length,
+		colLimit = grid[0].length;
+
+	for (let row = 0; row < rowLimit; row++) {
+		for (let col = 0; col < colLimit; col++) {
+			// console.log('grid=>', grid[row]);
+			// console.log('col=>', grid[row][col]);
+			let left = col - 1 < 0 ? 0 : grid[row][col - 1],
+				top = row - 1 < 0 ? 0 : grid[row - 1][col];
+
+			grid[row][col] += Math.max(left, top);
+		}
+	}
+
+	return grid[rowLimit - 1][colLimit - 1];
+};
+
 const grid = [
 	[1, 3, 1],
 	[1, 5, 1],
