a7e14433db0640ad77c753a2bf5a2a509bb94892
Saturday <zhangkairong123@qq.com>
2023-01-05 10:42:30 +0800

    easy 168.仅执行一次字符串交换能否使两个字符串相等 -> Map 🔫

--- 简单/168.仅执行一次字符串交换能否使两个字符串相等.js
+++ 简单/168.仅执行一次字符串交换能否使两个字符串相等.js
@@ -31,6 +31,47 @@ const question = `
 };
 
 
+/**
+ ** Map
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var areAlmostEqual2 = function (s1, s2) {
+	if (s1.length !== s2.length) return false;
+	if (s1 === s2) return true;
+
+	const map = {},
+		elementMap = {};
+
+	for (let index = 0; index < s1.length; index++) {
+		const element = s1[index];
+		map[index] = element;
+		elementMap[element] = (elementMap[element] || 0) + 1;
+	}
+
+	// return { map, elementMap };
+
+	let diff = 0;
+
+	for (let index = 0; index < s2.length; index++) {
+		const element = s2[index];
+		// elementMap
+		if (!elementMap[element]) return false;
+		elementMap[element] = elementMap[element] - 1;
+		if (elementMap[element] < 0) return false;
+		// map
+		const mapCurItem = map[index];
+		if (mapCurItem !== element) {
+			diff++;
+		}
+		if (diff > 2) return false;
+	}
+
+	return true;
+};
+
+
 console.log(areAlmostEqual('bank', 'kanb'));
 console.log(areAlmostEqual('attack', 'defend'));
 console.log(areAlmostEqual('kelb', 'kelb'));
