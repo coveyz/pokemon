91cb353f985ccf554c9df848cbfcd60d8e558d8f
Saturday <zhangkairong123@qq.com>
2023-01-19 10:50:50 +0800

    easy 186.实现 strStr() -> 双指针 🍪

--- 简单/186.实现 strStr().js
+++ 简单/186.实现 strStr().js
@@ -13,3 +13,32 @@ const question = `
 var strStr = function (haystack, needle) {
 	return haystack.indexOf(needle);
 };
+
+/**
+ ** 双指针
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+	if (haystack === needle) return 0;
+
+	let left = 0,
+		right = needle.length;
+
+	while (right <= haystack.length) {
+		const tmp = haystack.slice(left, right);
+
+		if (tmp === needle) return left;
+
+		left++;
+		right++;
+	}
+
+	return -1;
+};
+
+// const haystack = 'sadbutsad',needle = 'sad';
+const haystack = 'abc', needle = 'c';
+
+console.log(strStr(haystack, needle));
