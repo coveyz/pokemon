1a8817d62e8a9229b123d1a27cc82d03938967e0
Saturday <zhangkairong123@qq.com>
2023-02-06 11:08:02 +0800

    middle 78.N 叉树的层序遍历 -> BFS 🛥

--- 中等/78.N 叉树的层序遍历.js
+++ 中等/78.N 叉树的层序遍历.js
@@ -48,3 +48,48 @@ var levelOrder = function (root) {
 
 	return arr;
 };
+
+
+/**
+ * // Definition for a Node.
+ * function Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ ** BFS 
+ * @param {Node|null} root
+ * @return {number[][]}
+ */
+var levelOrder = function (root) {
+  if (!root) return [];
+  const queue = [[root,0]],res = [];
+
+  while (queue.length) {
+    const [node,level] = queue.shift();
+
+    // console.log('node=>',node);
+
+    if (!res[level]) {
+      res[level] = []
+    }
+    res[level].push(node.val);
+
+    // console.log('res=>',res);
+
+    if (node.children) {
+      for (let index = 0; index < node['children'].length; index++) {
+        const element = node['children'][index];
+        // console.log('node-children-element=>',element);
+        queue.push([element,level + 1]);
+      }
+    }
+
+    // console.log('queue=>',queue)
+  }
+
+  return res;
+};
+
