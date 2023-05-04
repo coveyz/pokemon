869044357efc2163aee7254bd56ed872c2ad8a17
Saturday <zhangkairong123@qq.com>
2023-05-04 10:59:29 +0800

    easy 21.移除元素 -> 双指针🧀

--- index.js
+++ index.js
@@ -1,35 +1,22 @@
 /**
- * @param {number[]} nums1
- * @param {number} m
- * @param {number[]} nums2
- * @param {number} n
- * @return {void} Do not return anything, modify nums1 in-place instead.
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
  */
-var merge = function (nums1, m, nums2, n) {
-	let p1 = 0,
-		p2 = 0,
-		cur;
-	const sorted = new Array(m + n).fill(0);
+var removeElement = function (nums, val) {
+	let left = 0,
+		right = nums.length;
 
-	while (p1 < m || p2 < n) {
-		if (p1 === m) {
-			cur = nums2[p2++];
-		} else if (p2 === n) {
-			cur = nums1[p1++];
-		} else if (nums1[p1] > nums2[p2]) {
-			cur = nums2[p2++];
+	while (left < right) {
+		if (nums[left] === val) {
+			nums[left] = nums[right - 1];
+			right--;
 		} else {
-			cur = nums1[p1++];
+			left++;
 		}
-
-		sorted[p1 + p2 - 1] = cur;
 	}
 
-	console.log(sorted);
-
-	for (let index = 0; index < m + n; index++) {
-		nums1[index] = sorted[index];
-	}
+	return left;
 };
 
-console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3));
+console.log(removeElement([3, 2, 2, 3], 3));
