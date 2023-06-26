a79d442e55ded6c478003d9140483d28ffc87bb9
Saturday <zhangkairong123@qq.com>
2023-06-26 10:54:37 +0800

    easy 299.二叉树的层平均值 -> DFS 🛹

--- index.js
+++ index.js
@@ -1,68 +1,39 @@
-var Trie = function () {
-	this.children = {};
-};
-
 /**
- * @param {string} word
- * @return {void}
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
  */
-Trie.prototype.insert = function (word) {
-	let node = this.children;
-	// console.log('insert-node=>', JSON.stringify(node));
-	for (const ch of word) {
-		if (!node[ch]) {
-			node[ch] = {};
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var averageOfLevels = function (root) {
+	const sums = [],
+		counts = [];
+
+	const dfs = (node, level) => {
+		if (!node) return;
+
+		if (!sums[level]) {
+			sums[level] = [];
+			counts[level] = 0;
 		}
-		node = node[ch];
-	}
 
-	node.isEnd = true;
+		sums[level].push(node.val);
+		counts[level]++;
 
-	return node;
+		node.left && dfs(node.left, level + 1);
+		node.right && dfs(node.right, level + 1);
+	};
+
+	dfs(root, 0);
+
+	return sums.map((item.index) => {
+    const tmp = item.reduce((acc,cur) => acc += cur, 0);
+    item = tmp / counts[index];
+    return item
+  })
 };
-
-Trie.prototype.searchPrefix = function (prefix) {
-	let node = this.children;
-
-	for (const ch of prefix) {
-		if (!node[ch]) {
-			return false;
-		}
-		node = node[ch];
-	}
-
-	return node;
-};
-
-/**
- * @param {string} word
- * @return {boolean}
- */
-Trie.prototype.search = function (word) {
-	const node = this.searchPrefix(word);
-	return node !== undefined && node.isEnd !== undefined;
-};
-
-/**
- * @param {string} prefix
- * @return {boolean}
- */
-Trie.prototype.startsWith = function (prefix) {
-	return this.searchPrefix(prefix);
-};
-
-/**
- * Your Trie object will be instantiated and called as such:
- * var obj = new Trie()
- * obj.insert(word)
- * var param_2 = obj.search(word)
- * var param_3 = obj.startsWith(prefix)
- */
-
-var obj = new Trie();
-obj.insert('apple');
-console.log(obj.search('apple'));
-console.log(obj.search('app'));
-console.log(obj.startsWith('app'));
-obj.insert('app');
-console.log(obj.search('app'));
--- 简单/299.二叉树的层平均值.js
+++ 简单/299.二叉树的层平均值.js
@@ -39,3 +39,44 @@ var averageOfLevels = function (root) {
 		return acc;
 	}, []);
 };
+
+/**
+ ** DFS 深度优先
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var averageOfLevels = function (root) {
+	const sums = [],
+		counts = [];
+
+	const dfs = (node, level) => {
+		if (!node) return;
+
+		if (!sums[level]) {
+			sums[level] = [];
+			counts[level] = 0;
+		}
+
+		sums[level].push(node.val);
+		counts[level]++;
+
+		node.left && dfs(node.left, level + 1);
+		node.right && dfs(node.right, level + 1);
+	};
+
+	dfs(root, 0);
+
+	return sums.map((item, index) => {
+		const tmp = item.reduce((acc, cur) => (acc += cur), 0);
+		item = tmp / counts[index];
+		return item;
+	});
+};
