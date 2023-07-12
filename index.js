8b5d1b758a2545ed80b27147fa34871399ae01e9
Saturday <zhangkairong123@qq.com>
2023-07-12 14:04:21 +0800

    easy 2.罗马字转汉字 -> map🎥

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
 /**
  * @param {string} s
- * @param {string} t
- * @return {boolean}
+ * @return {number}
  */
-var isAnagram = function (s, t) {
-	if (s.length !== t.length) return false;
+var romanToInt = function (s) {
+	const map = {
+		I: 1,
+		V: 5,
+		X: 10,
+		L: 50,
+		C: 100,
+		D: 500,
+		M: 1000,
+	};
 
-	const map = new Map();
+	let result = map[s[0]];
 
-	for (let index = 0; index < s.length; index++) {
-		const element = s[index];
-		map.set(element, map.has(element) ? map.get(element) + 1 : 1);
+	for (let index = 1; index < s.length; index++) {
+		const element = map[s[index]],
+			preElement = map[s[index - 1]];
+
+		console.log({ preElement, element });
+
+		if (element > preElement) {
+			result += (element - preElement - preElement)
+		} else {
+			result += element;
+		}
 	}
 
-	for (let index = 0; index < t.length; index++) {
-		const element = t[index];
-		if (!map.has(element)) return false;
-
-		map.set(element, map.get(element) - 1);
-
-		if (map.get(element) < 0) return false;
-	}
-
-	return true;
+	return result;
 };
 
-console.log(isAnagram('anagram', 'nagaram'));
-console.log(isAnagram('rat', 'car'));
+console.log(romanToInt('III'));
+console.log(romanToInt('IV'));
+console.log(romanToInt('MCMXCIV'));
+console.log(romanToInt('IX'));
