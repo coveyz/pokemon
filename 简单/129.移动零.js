bb47b5720f6dc88cf5d9556efa353048b178d7b8
Saturday <zhangkairong123@qq.com>
2024-03-06 15:10:07 +0800

    easy 移动零 -> splice 🏓

--- 简单/129.移动零.js
+++ 简单/129.移动零.js
@@ -19,5 +19,26 @@ var moveZeroes = function (nums) {
 	return nums;
 };
 
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let left = 0, right = nums.length - 1;
+
+    while(left < right) {
+        if (nums[left] === 0) {
+            nums.splice(left, 1);
+            nums.push(0);
+            right--;
+        } else {
+            left++
+        }
+    }
+
+    return nums;
+};
+
 console.log(moveZeroes([0, 2, 0, 1]));
 console.log(moveZeroes([0, 1, 0, 3, 12]));
--- 简单/302.字符串的最大公因子.js
+++ 简单/302.字符串的最大公因子.js
@@ -10,16 +10,20 @@ const question = `
  * @return {string}
  */
 var gcdOfStrings = function(str1, str2) {
+    // 判断两个字符串拼接的结果是否相等，如果不相等，则没有最大公因子，返回空字符串
     if (str1 + str2 !== str2 + str1)  return '';
 
+    // 定义一个求最大公约数的函数
     const gcd = (a, b) => {
+        // 如果b等于0，说明a就是最大公约数，返回a
         if (b === 0) return a;
+        // 否则，递归调用gcd函数，将b和a除以b的余数作为参数传入
         return gcd(b, a % b);
     }
 
+    // 返回str1的前gcd(str1.length, str2.length)个字符作为最大公因子
     return str1.substring(0, gcd(str1.length, str2.length));
 };
-
 // const str1 = "ABCABC", str2 = "ABC";
 const str1 = "ABABAB", str2 = "ABAB";
 
