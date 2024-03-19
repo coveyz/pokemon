f5640c7eb4a9428323e59752bddbe25eccd8a8e8
Saturday <zhangkairong123@qq.com>
2024-03-19 23:55:48 +0800

    middle 207.分隔链表 📮

---
+++ 中等/207.分隔链表.js
@@ -0,0 +1,38 @@
+const question = `
+给你一个链表的头节点 head 和一个特定值 x ，请你对链表进行分隔，使得所有 小于 x 的节点都出现在 大于或等于 x 的节点之前。
+你应当 保留 两个分区中每个节点的初始相对位置。
+`
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+var partition = function(head, x) {
+    const dummy1 = new ListNode(0), dummy2 = new ListNode(0);
+
+    let small = dummy1, large = dummy2;
+
+    while(head) {
+        if (head.val < x) {
+            small.next = head;
+            small = small.next;
+        } else {
+            large.next = head;
+            large = large.next;
+        }
+        head = head.next;
+    }
+
+    small.next = dummy2.next;
+    large.next = null;
+
+    return dummy1.next;
+};
