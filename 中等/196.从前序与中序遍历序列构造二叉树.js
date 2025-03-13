b8e2ee859d2c7baa31abe30c5899b5ff85b7b1d1
Saturday <zhangkairong123@qq.com>
2025-03-13 11:17:09 +0800

    middle 196.从前序与中序遍历序列构造二叉树 -> 哈希 🌎

--- 中等/196.从前序与中序遍历序列构造二叉树.js
+++ 中等/196.从前序与中序遍历序列构造二叉树.js
@@ -23,10 +23,47 @@ var buildTree = function(preorder, inorder) {
 
     // 中序遍历中找根节点
     const rootIndex = inorder.indexOf(rootVal);
-    console.log('left=>', preorder.slice(1, rootIndex + 1), inorder.slice(0, rootIndex))
-    console.log('right=>', preorder.slice(rootIndex + 1), inorder.slice(rootIndex + 1))
     root.left = buildTree(preorder.slice(1, rootIndex + 1), inorder.slice(0, rootIndex));
     root.right = buildTree(preorder.slice(rootIndex + 1), inorder.slice(rootIndex + 1));
 
     return root;
+};
+
+
+/**
+ * 用哈希表加速
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function(preorder, inorder) {
+    const inorderMap = new Map();
+    inorder.forEach((val, index) => {
+        inorderMap.set(val, index);
+    });
+
+    let preIndex = 0;
+
+    const build = (left, right) =>{
+        if (left > right) return null;
+
+        let rootVal = preorder[preIndex++];
+        let root = new TreeNode(rootVal);
+        let rootIndex = inorderMap.get(rootVal);
+
+        root.left = build(left, rootIndex - 1);
+        root.right = build(rootIndex + 1, right);
+
+        return root;
+    };
+
+    return build(0, inorder.length - 1);
 };
