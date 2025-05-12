602b7261a8040dcff946ac16cf2d621410ad5faa
Saturday <zhangkairong123@qq.com>
2025-05-12 10:42:30 +0800

    easy 131.数组中出现次数超过一半的数字 -> 摩尔投票法 🕹️

--- 简单/131.数组中出现次数超过一半的数字.js
+++ 简单/131.数组中出现次数超过一半的数字.js
@@ -1,6 +1,6 @@
 const question = `数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。你可以假设数组是非空的，并且给定的数组总是存在多数元素。`;
 
-var majorityElement = function (nums) {
+var majorityElement1 = function (nums) {
 	if (nums.length === 1) return nums[0];
 	const obj = {};
 	const len = Math.ceil(nums.length / 2);
@@ -20,7 +20,7 @@ var majorityElement = function (nums) {
 
 
 //* map
-var majorityElement = function (nums) {
+var majorityElementMap = function (nums) {
 	const number = Math.floor(nums.length / 2),
 		map = new Map();
 
@@ -38,6 +38,26 @@ var majorityElement = function (nums) {
 
 	return map;
 };
+
+/**
+ ** 摩尔投票法
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let count = 0, candidate = null;
+
+    for(const num of nums) {
+        if (count === 0) {
+            candidate = num;
+        };
+
+        count += (num === candidate) ? 1 : -1;
+    }
+
+    return candidate;
+};
+
 
 console.log(majorityElement([1, 2, 3, 2, 2, 2, 5, 4, 2]));
 console.log(majorityElement([1, 2, 1]));
