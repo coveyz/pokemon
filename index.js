b154832bcdc9c24c4f43ace4ed447448c9de8b6b
Saturday <zhangkairong123@qq.com>
2025-08-13 16:46:06 +0800

    easy 231. 字符串中的第一个唯一字符 -> 队列🚔

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 /**
  * @param {string} s
- * @return {boolean}
+ * @return {number}
  */
-var isValid = function(s) {
-    const map = {'(': 1, ')': -1, '{': 2, "}": -2, '[': 3, ']': -3},
-        stack = [];
+var firstUniqChar = function(s) {
+    const cnt = new Map(), 
+        queue = [];
+    let head = 0;
 
     for (let i = 0; i < s.length; i++) {
-        
-        const element = s[i], elementCount = map[element];
-        if (elementCount > 0) stack.push(elementCount);
-        else {
-            const lastElementCount = stack.pop();
-            if (lastElementCount + elementCount !== 0) return false;
-        }
-    }
+        const ch = s[i];
+        cnt.set(ch, (cnt.get(ch) || 0)+ 1);
+        queue.push([ch, i]);
 
-    return stack.length === 0;
+        while(head < queue.length && cnt.get(queue[head][0]) > 1) head++;
+    };
+
+    return head < queue.length ? queue[head][1] : -1;
 };
 
-console.log(isValid("()")); // true
+
+console.log(firstUniqChar("leetcode")); // 0
+// console.log(firstUniqChar("loveleetcode")); // 2
+// console.log(firstUniqChar("aabb")); // -1
+// console.log(firstUniqChar("")); // -1
+// console.log(firstUniqChar("a")); // 0
+// console.log(firstUniqChar("ab")); // 0
--- 简单/231. 字符串中的第一个唯一字符.js
+++ 简单/231. 字符串中的第一个唯一字符.js
@@ -46,5 +46,26 @@ var firstUniqChar = function(s) {
 	return map.size > 0 ? map.values().next().value : -1
 };
 
+/**
+ * 队列解法
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    const cnt = new Map(), 
+        queue = [];
+    let head = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const ch = s[i];
+        cnt.set(ch, (cnt.get(ch) || 0)+ 1);
+        queue.push([ch, i]);
+
+        while(head < queue.length && cnt.get(queue[head][0]) > 1) head++;
+    };
+
+    return head < queue.length ? queue[head][1] : -1;
+};
+
 console.log(firstUniqChar('leetcode'));
 console.log(firstUniqChar('loveleetcode'));
