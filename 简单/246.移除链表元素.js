6b90dc724c285c5fcdfb9efa4cb8315ec4d49cc2
Saturday <zhangkairong123@qq.com>
2025-09-01 11:52:40 +0800

    easy 246.移除链表元素 -> 迭代 🔁

--- 简单/246.移除链表元素.js
+++ 简单/246.移除链表元素.js
@@ -20,3 +20,31 @@ const question = `
 
   return head.val === val ? head.next : head.val
 };
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+    const dummy = new ListNode(0, head);
+    let cur = dummy;
+
+    while(cur.next) {
+        if (cur.next.val === val) {
+            cur.next =cur.next.next;
+        } else {
+            cur = cur.next;
+        }
+    }
+
+    return dummy.next;
+};
--- 简单/249.二叉树的中序遍历.js
+++ 简单/249.二叉树的中序遍历.js
@@ -22,3 +22,36 @@ var inorderTraversal = function (root, arr = []) {
 
 	return arr;
 };
+
+
+
+/**
+ * 迭代
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    const stack = [],
+        result =[];
+    let current = root;
+
+    while(current || stack.length) {
+        while(current) {
+            stack.push(current);
+            current = current.left;
+        };
+        current = stack.pop();
+        result.push(current.val);
+        current = current.right;
+    }
+
+    return result;
+};
