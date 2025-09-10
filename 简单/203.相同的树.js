23d235b0d33a50a4696e1b6b16518accdd4d204b
Saturday <zhangkairong123@qq.com>
2025-09-10 14:46:22 +0800

    easy 256. 对称二叉树 -> 广度优先思路🏂

--- 简单/203.相同的树.js
+++ 简单/203.相同的树.js
@@ -65,3 +65,34 @@ var isSameTree = function (p, q) {
 
 	return queueP.length === queueQ.length;
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * BFS
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+    const queue = [[p, q]];
+
+    while(queue.length) {
+        const [p, q] = queue.shift();
+        if (!p && !q) continue;
+        if (!p || !q) return false;
+        if (p.val !== q.val) return false;
+
+        queue.push([p.left, q.left]);
+        queue.push([p.right, q.right]);
+    }
+
+    return true;
+};
--- 简单/256. 对称二叉树.js
+++ 简单/256. 对称二叉树.js
@@ -24,3 +24,36 @@ var isSymmetric = function (root) {
 
 	return check(root, root);
 };
+
+
+
+/**
+ * BFS 广度优先搜索
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    if (!root) return false;
+    const queue = [[root.left, root.right]];
+
+    while(queue.length) {
+        const [leftNode, rightNode] = queue.shift();
+
+        if (!leftNode && !rightNode) continue;
+        else if (!leftNode || !rightNode) return false;
+        if (leftNode.val !== rightNode.val) return false;
+
+        queue.push([leftNode.left, rightNode.right]);
+        queue.push([leftNode.right, rightNode.left]);
+    };
+
+    return true;
+};
