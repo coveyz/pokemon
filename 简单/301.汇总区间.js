f0c5a6616ab859aa71c9e67f8d3a8d938d7ba001
coveyz <zhangkairong123@qq.com>
2025-11-11 11:11:25 +0800

    easy 301.汇总区间 -> 简洁方案 🍳

--- 简单/301.汇总区间.js
+++ 简单/301.汇总区间.js
@@ -43,6 +43,32 @@ var summaryRanges = function (nums) {
 	return result;
 };
 
+/**
+ ** 简洁方法
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+var summaryRanges = function(nums) {
+    if (!nums.length) return [];
+
+    const result = [];
+    let start = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (i === nums.length - 1 || nums[i + 1] !== nums[i] + 1) {
+            if (start === i) {
+                result.push(nums[start] + '');
+            } else {
+                result.push(`${nums[start]}->${nums[i]}`);
+            };
+            start = i + 1;
+        }
+    }
+
+
+    return result;
+};
+
 console.log(summaryRanges([0, 1, 2, 4, 5, 7]));
 console.log(summaryRanges([0, 2, 3, 4, 6, 8, 9]));
 console.log(summaryRanges([]));
