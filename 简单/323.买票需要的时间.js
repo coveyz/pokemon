8a8e8eb90ebd0aba74a41f9491b69654fa4c0aec
coveyz <zhangkairong123@qq.com>
2026-03-03 11:27:30 +0800

    easy 323.买票需要的时间 -> 模拟🏂

--- 简单/323.买票需要的时间.js
+++ 简单/323.买票需要的时间.js
@@ -27,3 +27,26 @@ var timeRequiredToBuy = function(tickets, k) {
 
     return time;
 };
+
+
+/**
+ ** 模拟
+ * @param {number[]} tickets
+ * @param {number} k
+ * @return {number}
+ */
+var timeRequiredToBuy = function(tickets, k) {
+    let time = 0;
+
+    while(tickets[k] > 0) {
+        for (let i = 0; i < tickets.length; i++) {
+            if (tickets[i] > 0) {
+                tickets[i]--;
+                time++;
+
+                if (i === k && tickets[k] === 0) return time;
+            }
+        }
+    }
+
+};
